a03872b7d15fd64e7a0f2043a445af197348fe2c
Lukas Wunner <lukas@wunner.de>
2020-02-10 15:41:55 +0100

    Drop files of non-kernel packages
    Commit 09c901820fe2 ("Build only kernel packages") removed all packages
    from debian/control except raspberrypi-kernel and
    raspberrypi-kernel-headers.
    
    However the commit retained the (now obsolete) files of the removed
    packages.  Drop them.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

--- debian/gen_bootloader_postinst_preinst.sh
+++ debian/gen_bootloader_postinst_preinst.sh
@@ -76,54 +76,6 @@ EOF
 printf "#DEBHELPER#\n" >> raspberrypi-kernel.postinst
 printf "#DEBHELPER#\n" >> raspberrypi-kernel.preinst
 
-printf "#!/bin/sh\n" > raspberrypi-bootloader.postinst
-printf "#!/bin/sh\n" > raspberrypi-bootloader.preinst
-
-printf "mkdir -p /usr/share/rpikernelhack\n" >> raspberrypi-bootloader.preinst
-
-cat <<EOF >> raspberrypi-bootloader.preinst
-if [ -f "/boot/recovery.elf" ]; then
-  echo "/boot appears to be NOOBS recovery partition. Applying fix."
-  rootnum=\`cat /proc/cmdline | sed -n 's|.*root=/dev/mmcblk0p\([0-9]*\).*|\1|p'\`
-  if [ ! "\$rootnum" ];then
-    echo "Could not determine root partition"
-    exit 1
-  fi
-
-  if ! grep -qE "/dev/mmcblk0p1\s+/boot" /etc/fstab; then
-    echo "Unexpected fstab entry"
-    exit 1
-  fi
-
-  boot="/dev/mmcblk0p\$((rootnum-1))"
-  root="/dev/mmcblk0p\${rootnum}"
-  sed /etc/fstab -i -e "s|^.* / |\${root}  / |"
-  sed /etc/fstab -i -e "s|^.* /boot |\${boot}  /boot |"
-  umount /boot
-  if [ \$? -ne 0 ]; then
-    echo "Failed to umount /boot. Remount manually and run sudo apt-get install -f."
-    exit 1
-  else
-    mount /boot
-  fi
-fi
-
-EOF
-
-for FN in ../boot/*.elf ../boot/*.dat ../boot/*.bin ../boot/LICENCE.broadcom; do
-  if ! [ -d "$FN" ]; then
-    FN=${FN#../boot/}
-    printf "rm -f /boot/$FN\n" >> raspberrypi-bootloader.postinst
-    printf "dpkg-divert --package rpikernelhack --remove --rename /boot/$FN\n" >> raspberrypi-bootloader.postinst
-    printf "sync\n" >> raspberrypi-bootloader.postinst
-
-    printf "dpkg-divert --package rpikernelhack --divert /usr/share/rpikernelhack/$FN /boot/$FN\n" >> raspberrypi-bootloader.preinst
-  fi
-done
-
-printf "#DEBHELPER#\n" >> raspberrypi-bootloader.postinst
-printf "#DEBHELPER#\n" >> raspberrypi-bootloader.preinst
-
 printf "#!/bin/sh\n" > raspberrypi-kernel.prerm
 printf "#!/bin/sh\n" > raspberrypi-kernel.postrm
 printf "#!/bin/sh\n" > raspberrypi-kernel-headers.postinst
