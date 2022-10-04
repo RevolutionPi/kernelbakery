ec402300a8b0a52f45539a3c3cfff961ff6a29ed
Nicolai Buchwitz <n.buchwitz@kunbus.com>
2022-10-04 16:21:29 +0200

    Allow for optional arm64 builds
    In order to build an optional arm64 kernel with the same toolchain,
    the existing build scripts have been modified. With this it is possible
    to specify the target architecture via the env var `ARCH`. If `ARCH` is
    not specified, the build script assumes `arm` as default (keep previous
    behaviour). If `ARCH=arm64` is specified, the build scripts builds only
    one kernel (`kernel8.img`) which can be used for CM3 as well as CM4S. It
    is now also possible to explicitly override the CROSS_COMPILE var from
    the environment. This can be helpfull if the kernel is build on target
    architecture.
    
    Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

--- debian/update.sh
+++ debian/update.sh
@@ -8,19 +8,19 @@ copy_files (){
 	rsync -aHAX \
 		--files-from=<(cd linux; find . -name Makefile\* -o -name Kconfig\* -o -name \*.pl | grep -E -v '^\./debian') linux/ "$destdir/"
 	rsync -aHAX \
-		--files-from=<(cd linux; find arch/arm/include include scripts -type f) linux/ "$destdir/"
+		--files-from=<(cd linux; find arch/${ARCH}/include include scripts -type f) linux/ "$destdir/"
 	rsync -aHAX \
-		--files-from=<(cd linux; find arch/arm -name module.lds -o -name Kbuild.platforms -o -name Platform) linux/ "$destdir/"
+		--files-from=<(cd linux; find arch/${ARCH} -name module.lds -o -name Kbuild.platforms -o -name Platform) linux/ "$destdir/"
 	rsync -aHAX \
 		--files-from=<( \
 				cd linux; \
-				find arch/arm -name include -type d -print0 -o -name scripts -type d -print0 | \
+				find arch/${ARCH} -name include -type d -print0 -o -name scripts -type d -print0 | \
 				xargs -0 -I '{}' find '{}' -type f \
 			) \
 			linux/ \
 			"$destdir/"
 	rsync -aHAX \
-		--files-from=<(cd "$builddir"; find arch/arm/include Module.symvers .config include scripts -type f) "$builddir" "$destdir/"
+		--files-from=<(cd "$builddir"; find arch/${ARCH}/include Module.symvers .config include scripts -type f) "$builddir" "$destdir/"
 	find "$destdir/scripts" -type f -exec file {} + | grep -E 'ELF .* x86-64' | cut -d: -f1 | xargs rm
 	find "$destdir/scripts" -type f -name '*.cmd' -exec rm {} +
 	ln -sf "/usr/src/linux-headers-$version" "headers/lib/modules/$version/build"
@@ -45,6 +45,24 @@ if [ -n "$PIKERNELMODDIR" ] ; then
     fi
 fi
 
+ARCH=${ARCH:-arm}
+case "$ARCH" in
+	arm)
+		CROSS_COMPILE=${CROSS_COMPILE:-arm-linux-gnueabihf-}
+		# CM1=6 CM3=7 CM4=7l (32 bit kernel)
+		kernel_versions="6 7 7l"
+		;;
+	arm64)
+		CROSS_COMPILE=${CROSS_COMPILE:-aarch64-linux-gnu-}
+		# CM3/CM4S/CM4=8 (64 bit kernel)
+		kernel_versions="8"
+		;;
+	*)
+		echo 1>&2 "Unsupported architecture: ${ARCH}"
+		exit 1
+		;;
+esac
+
 INSTDIR=$(dirname "$0")
 if [ "${INSTDIR#/}" == "$INSTDIR" ] ; then INSTDIR="$PWD/$INSTDIR" ; fi
 INSTDIR=${INSTDIR%%/debian}
@@ -52,24 +70,24 @@ BUILDDIR_TEMPLATE=$INSTDIR/kbuild
 export KBUILD_BUILD_TIMESTAMP=$(date --rfc-2822)
 export KBUILD_BUILD_USER="support"
 export KBUILD_BUILD_HOST="kunbus.com"
-make_opts=(CFLAGS_KERNEL='-fdebug-prefix-map=$LINUXDIR=.' CFLAGS_MODULE='-fdebug-prefix-map=$LINUXDIR=.' ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- O="$BUILDDIR_TEMPLATE")
+export ARCH
+export CROSS_COMPILE
+make_opts=(CFLAGS_KERNEL='-fdebug-prefix-map=$LINUXDIR=.' CFLAGS_MODULE='-fdebug-prefix-map=$LINUXDIR=.' O="$BUILDDIR_TEMPLATE")
 
 if [ ! -L "$INSTDIR/linux" ] ; then
     ln -sf "$LINUXDIR" "$INSTDIR/linux"
 fi
-rm -rf "$INSTDIR/headers"
 
 [ -d "$INSTDIR/boot/overlays" ] || mkdir "$INSTDIR/boot/overlays"
-rm -f "$INSTDIR/boot"/*.dtb "$INSTDIR/boot/overlays"/*.dtbo
 
+# cleanup from previous builds
+rm -rf "$INSTDIR/headers"
+rm -f "$INSTDIR/boot/broadcom"/*.dtb "$INSTDIR/boot"/*.dtb "$INSTDIR/boot/overlays"/*.dtbo "$INSTDIR/boot"/kernel*.img
 rm -rf modules/*
-
-# CM1=6 CM3=7 CM4=7l
-kernel_versions="6 7 7l"
 
 for kernel_version in $kernel_versions; do
     defconfig="revpi-v${kernel_version}_defconfig"
-    test -f "linux/arch/arm/configs/$defconfig" || continue
+    test -f "linux/arch/${ARCH}/configs/$defconfig" || continue
     builddir=${BUILDDIR_TEMPLATE}${kernel_version/6/}
     make_opts[-1]="O=${builddir}"
 
@@ -78,7 +96,11 @@ for kernel_version in $kernel_versions; do
 
     # build kernel
     (cd linux; make "${make_opts[@]}" $defconfig)
-    (cd linux; make "${make_opts[@]}" -j$NPROC zImage modules 2>&1)
+    if [ "$ARCH" == "arm64" ]; then
+	    (cd linux; make "${make_opts[@]}" -j$NPROC Image modules 2>&1)
+    else
+	    (cd linux; make "${make_opts[@]}" -j$NPROC zImage modules 2>&1)
+    fi
     version="$(cat "$builddir/include/config/kernel.release")"
     copy_files "$builddir"
 
@@ -91,7 +113,15 @@ for kernel_version in $kernel_versions; do
     fi
 
     # install kernel
-    cp "$builddir/arch/arm/boot/zImage" "$INSTDIR/boot/kernel${kernel_version/6/}.img"
+
+    if [ "$ARCH" == "arm64" ]; then
+	    # arm64 kernel is uncompressed by default, let's gzip it
+	    cp "$builddir/arch/${ARCH}/boot/Image" "$INSTDIR/boot/kernel8.img"
+	    gzip "$INSTDIR/boot/kernel8.img"
+	    mv "$INSTDIR/boot/kernel8.img"{.gz,}
+    else
+	    cp "$builddir/arch/${ARCH}/boot/zImage" "$INSTDIR/boot/kernel${kernel_version/6/}.img"
+    fi
 
     # install modules
     if [ -d "$PIKERNELMODDIR" ] ; then
@@ -106,9 +136,13 @@ done
 # install dtbs (based on last builddir)
 (cd linux; make "${make_opts[@]}" -j$NPROC dtbs 2>&1)
 (cd linux; make "${make_opts[@]}" -j$NPROC dtbs_install INSTALL_DTBS_PATH=/tmp/dtb.$$)
-mv /tmp/dtb.$$/*.dtb "$INSTDIR/boot"
+if [ "$ARCH" == "arm64" ]; then
+	mv /tmp/dtb.$$/broadcom/*.dtb "$INSTDIR/boot"
+else
+	mv /tmp/dtb.$$/*.dtb "$INSTDIR/boot"
+fi
 mv /tmp/dtb.$$/overlays/* "$INSTDIR/boot/overlays"
-rmdir /tmp/dtb.$$/overlays /tmp/dtb.$$
+rmdir /tmp/dtb.$$/* /tmp/dtb.$$
 
 [ ! -d "extra" ] && mkdir "extra"
 echo "_ _ $version" > extra/uname_string
